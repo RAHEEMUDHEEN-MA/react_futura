e0f8c80604456e26a88267fb1ba499b8a1e88a32
RAHEEMUDHEEN-MA <raheeemudheenma@gmail.com>
2023-12-12 12:19:58 +0530

    respoonsive todo

--- src/Test_works/ToDo/ToDoData.js
+++ src/Test_works/ToDo/ToDoData.js
@@ -1,14 +1,73 @@
-const ToDodataArray=[
-    {id:1,date:"21/02/2022",time:"12:40",todo:"buy a cake🍰" ,status:"pending",},
-    {id:2,date:"01/10/2254",time:"3:55",todo:"ABC Task",status:"pending"},
-    {id:3,date:"01/10/2254",time:"10:07",todo:"nothing aple abg noom gate rate dfsshbjhdsfbdjshgjdfbjgb",status:"deleted"},
-    {id:4,date:"01/10/2254",time:"01:21",todo:"money 💵",status:"pending"},
-    {id:5,date:"21/02/2022",time:"12:00",todo:"brought a pan",status:"completed"},
-    {id:6,date:"01/10/2254",time:"7:00",todo:"shot",status:"completed"},
-    {id:7,date:"21/02/2022",time:"12:52",todo:"brought a car",status:"deleted"},
-    {id:7,date:"21/02/2022",time:"1:25",todo:"ABCDEFG HIJKLMNP,QE",status:"deleted"},
-    {id:4,date:"01/10/2254",time:"4:00",todo:"Movie ",status:"pending"},
-
-    
-]
-export default ToDodataArray
+const ToDodataArray = [
+  {
+    id: 1,
+    date: "21/02/2022",
+    time: "12:40",
+    todo: "buy a cake🍰",
+    status: "pending",
+  },
+  {
+    id: 2,
+    date: "01/10/2254",
+    time: "3:55",
+    todo: "ABC Task",
+    status: "pending",
+  },
+  {
+    id: 3,
+    date: "01/10/2254",
+    time: "10:07",
+    todo: "nothing aple abg noom gate rate dfsshbjhdsfbdjshgjdfbjgb",
+    status: "deleted",
+  },
+  {
+    id: 4,
+    date: "01/10/2254",
+    time: "01:21",
+    todo: "money 💵",
+    status: "pending",
+  },
+  {
+    id: 5,
+    date: "21/02/2022",
+    time: "12:00",
+    todo: "brought a pan",
+    status: "completed",
+  },
+  {
+    id: 6,
+    date: "01/10/2254",
+    time: "7:00",
+    todo: "shot",
+    status: "completed",
+  },
+  {
+    id: 6,
+    date: "01/10/2254",
+    time: "7:00",
+    todo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita error commodi nisi inventore  illo?",
+    status: "completed",
+  },
+  {
+    id: 7,
+    date: "21/02/2022",
+    time: "12:52",
+    todo: "brought a car",
+    status: "deleted",
+  },
+  {
+    id: 7,
+    date: "21/02/2022",
+    time: "1:25",
+    todo: "ABCDEFG HIJKLMNP,QE",
+    status: "deleted",
+  },
+  {
+    id: 4,
+    date: "01/10/2254",
+    time: "4:00",
+    todo: "Movie ",
+    status: "pending",
+  },
+];
+export default ToDodataArray;
